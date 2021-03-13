d0a021154c0f74dd10680334bf1692623f6fa772
byom26 <p.byom26@gmail.com>
2021-03-13 14:14:32 +0530

    added Update and delete API endpoints

--- blog/main.py
+++ blog/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI, Depends, status, Response, HTTPException
-from starlette.status import HTTP_404_NOT_FOUND
 from . import schemas,models
 from .database import engine, sessionLocal
 from sqlalchemy.orm import Session
+from typing import List
 
 
 app = FastAPI()
@@ -24,12 +24,30 @@ def create(request:schemas.Blog, db:Session=Depends(get_db)):
     db.refresh(new_blog)
     return new_blog
 
-@app.get('/blog')
+@app.delete('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+def destroy(id, db:Session=Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} doesnot exists!")
+    blog.delete(synchronize_session=False)
+    db.commit()
+    return f'Blog with id {id} deleted successfully!'
+
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update(id, request:schemas.Blog, db:Session=Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} doesnot exists!")
+    blog.update(request)
+    db.commit()
+    return "Blog updated successfully!"
+
+@app.get('/blog', response_model=List[schemas.ShowBlog])
 def all(db:Session=Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
-@app.get('/blog/{id}', status_code=status.HTTP_200_OK)
+@app.get('/blog/{id}', status_code=status.HTTP_200_OK, response_model=schemas.ShowBlog) #-- response_model is used to limit the data that will be shown to the user. More details can be found inside schemas.py file
 def show(id, response:Response, db:Session=Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
--- blog/schemas.py
+++ blog/schemas.py
@@ -4,3 +4,19 @@ from pydantic import BaseModel
 class Blog(BaseModel):
     title: str
     body: str
+
+# class ShowBlog(Blog):
+    
+#     class Config():
+#         orm_mode = True
+
+#-- We can either extend the Blog class as above to hide the id from the user
+#-- OR We can extend the BaseModel as below to hide the id from the user but here we have to manually declare the fields
+#-- Also don't forget to craete the orm_mode Config.
+
+class ShowBlog(BaseModel):
+    title: str
+    body: str
+    
+    class Config():
+        orm_mode = True
